b8e6c52f0490ee87fcae18f76cda5a1eefe2be7a
Ale <alessandrochb@gmail.com>
2020-12-22 16:23:58 +0100

    create dinamicamente le card della pasta suddivise per tipo

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -1,33 +1,20 @@
-
 @extends('layouts.app')
 
 @section('content')
-    <h1>prodotti</h1>
 
     <section class="pruducts-section">
         <div class="container">
-            <div class="card-container">
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
-                <div class="card">
-                    <img src="https://www.lamolisana.it/wp-content/uploads/2017/06/4-spaghetto-quadrato-bucato-m.jpg" alt="">
-                </div>
+            <div class="pruducts-section-container">
+                @foreach($shapes as $key => $shape)
+                    <h2 class="text-uppercase">{{ $key }}</h2>
+                    <div class="card-container">
+                        @foreach ($shape as $type)
+                            <div class="card">
+                                <img src="{{ $type['src'] }}" alt="{{ $type['titolo'] }}">
+                            </div>
+                        @endforeach
+                    </div>
+                @endforeach
             </div>
         </div>
     </section>
