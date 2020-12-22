1e806cec4e1e43a323fc5162219d0cafdb1b4bda
Ale <alessandrochb@gmail.com>
2020-12-22 15:06:06 +0100

    stilato header (valido per tutte le sezioni)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,6 +3,10 @@
     <head>
         <meta charset="utf-8">
         <title>titolo</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        {{-- google fonts --}}
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     </head>
     <body>
         <header>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,23 @@
-<h2>header</h2>
+{{-- <h2>header</h2> --}}
+<div class="header-container">
+    <div class="header-top">
+        <div class="logo-container">
+            <img src="{{ asset('/images/logo-la-molisana.png') }}" alt="">
+        </div>
+    </div>
+    <div class="header-bottom">
+        <div class="nav-menu">
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Prodotti</a>
+                </li>
+                <li>
+                    <a href="#">News</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
