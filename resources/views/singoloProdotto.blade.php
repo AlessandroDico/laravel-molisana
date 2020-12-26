8e1a85c8e0b548b69458f3f25218a3dc079ad655
Ale <alessandrochb@gmail.com>
2020-12-26 15:48:05 +0100

    stilata la pagina prodotto singolo

--- resources/views/singoloProdotto.blade.php
+++ resources/views/singoloProdotto.blade.php
@@ -1,11 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-    <p>{{$prodotto['titolo']}}</p>
+    <div class="single-product-title">
+        <h2>{{$prodotto['titolo']}}</h2>
+    </div>
     <div class="type-pasta-img">
         <img src="{{ $prodotto['src-h'] }}" alt="{{ $prodotto['titolo'] }}">
     </div>
     <div class="packaging-pasta-img">
         <img src="{{ $prodotto['src-p'] }}" alt="{{$prodotto['titolo'] . ' confezionato' }}">
     </div>
+@endsection
+
+@section('page-title')
+    {{$prodotto['titolo'] . ' - La Molisana'}} 
 @endsection
