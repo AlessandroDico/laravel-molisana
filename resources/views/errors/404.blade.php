d419a41b9d5a820fa447415b259a410bf87c4ecd
Ale <alessandrochb@gmail.com>
2020-12-29 11:15:25 +0100

    creata pagina 404 personalizzata

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>404 - Not Found</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    </head>
+    <body class="error-page-body">
+        <h1 id="error-fof-title">404 - Page Not Found!</h1>
+        <p id="error-fof-paragraph">sorry this page doesn't exist</p>
+    </body>
+</html>
